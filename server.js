2d495ad221871e9ef24271bcdd9ee8ca9fcd2f5b
Daniel Boldan <boldan.daniel@gmail.com>
2016-11-27 04:01:58 +0200

    Creating a wildcard search

--- server.js
+++ server.js
@@ -6,43 +6,42 @@ var app = express();
 var morgan = require("morgan");
 var cors = require("cors");
 
-app.use( cors( { credentials: true, origin: true } ) );
+app.use(cors({ credentials: true, origin: true }));
 
-var mongoose = require( "mongoose" );
+var mongoose = require("mongoose");
 
-mongoose.connect( "mongodb://localhost/websearchapi/sites" );
+mongoose.connect("mongodb://localhost/websearchapi/sites");
 
-var siteSchema = new mongoose.Schema( {
+var siteSchema = new mongoose.Schema({
     title: String,
     link: String,
     desc: String
-} );
+});
 
-var searchDb = mongoose.model( 'sites', siteSchema );
+var searchDb = mongoose.model('sites', siteSchema);
 
-var routes = function( app ) {
-    app.use( bodyparser.json() );
+var routes = function(app) {
+    app.use(bodyparser.json() );
 
-    app.get( '/search/:title', function( req, res ) {
-        searchDb.find( { title: req.params.title }, function( err, data ) {
-            if ( err ) return res.status(500)
-                .send( {
-                    'msg': 'couldn\'t find anything'
-                } );
-
-            res.json( data );
-        } );
+    app.get('/search/:title', function(req, res) {
+        searchDb.find( {
+                title: { $regex: req.params.title + '*', $options: 'ix' }
+            },
+            function(err, data) {
+                res.json(data);
+            }
+        );
     });
 };
 
 var router = express.Router();
 
-routes( router );
+routes(router);
 
-app.use( '/v1', router );
+app.use('/v1', router);
 
 var port = process.env.PORT || 5000;
 
-app.listen( port, function() {
-    console.log( 'server listening on port ' + ( process.env.PORT || port ) );
+app.listen(port, function() {
+    console.log('server listening on port ' + (process.env.PORT || port));
 });
